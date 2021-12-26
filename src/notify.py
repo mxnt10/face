a17f809a69736bb4256027b032f6c41222f9740b
MauricioFerrari-NovaTrento <m10ferrari1200@gmail.com>
2021-12-26 18:32:03 -0300

    Update Version 2.1 - 2021/12/26

--- src/notify.py
+++ src/notify.py
@@ -12,6 +12,8 @@ from jsonTools import set_json
 from utils import setIcon, setSound
 from version import __pagename__
 
+varClass = 'bp9cbjyn ljqsnud1 pq6dq46d datstx6m taijpn5t jb3vyjys jxrgncrl qt6c0cv9 qnrpqo6b k4urcfbm'
+
 
 ########################################################################################################################
 
@@ -19,9 +21,9 @@ from version import __pagename__
 # Função para exibição de notificação.
 def notifyMessage(self):
     if self.soma > 1:
-        ms = 'Unread messages.'
+        ms = 'notifications not seen.'
     else:
-        ms = 'Unread message.'
+        ms = 'notification not seen.'
     com = 'notify-send --app-name="' + __pagename__ + '" --expire-time=' + str(set_json('TimeMessage')) + \
           ' --icon="' + realpath(setIcon('notify')) + '" "' + str(self.soma) + ' ' + ms + '"'
     run(com, shell=True)
@@ -30,13 +32,8 @@ def notifyMessage(self):
 # Essa função pode variar conforme o webapp.
 def verifyNotify(self, res):
     self.soma = 0
-    tags = res.findAll(
-        "span",
-        {"class": "bp9cbjyn ljqsnud1 pq6dq46d datstx6m taijpn5t jb3vyjys jxrgncrl qt6c0cv9 qnrpqo6b k4urcfbm"}
-    )
-
-    for tag in tags:
-        self.soma += int(tag.getText())  # Contabilizando o número de mensagens
+    for tag in res.xpath('//span[@class="' + varClass + '"]'):
+        self.soma += int(tag.text)
     if self.soma != self.notify and self.soma != 0:
         if self.isHidden() or int(self.windowState()) == 1 or int(self.windowState()) == 3:
             if set_json('NotifySound'):
