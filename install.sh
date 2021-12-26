a7f6cdf8ed0c5155d7460ab1afdbf0d075b86391
MauricioFerrari-NovaTrento <m10ferrari1200@gmail.com>
2021-12-26 15:47:08 -0300

    Update Version 2.1 - 2021/12/26

--- install.sh
+++ install.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-pkgver='1.3'
+PRGNAM="face"
+VERSION=$(< RELEASE)
 install_root=${install_root:-""}
 
 set -e
-# shellcheck disable=SC2015
 [ "$install_root" != "" ] && {
-  mkdir -p "$install_root"/usr/{bin,share/{applications,pixmaps,face/icon_status},doc/face-"$pkgver"}
+  mkdir -p "$install_root"/usr/{bin,share/{applications,pixmaps,"$PRGNAM"/{icon_status,sound}},doc/"$PRGNAM"-"$VERSION"}
 } || {
-  mkdir -p /usr/{share/face/icon_status,doc/face-"$pkgver"}
+  mkdir -p /usr/{share/"$PRGNAM"/{icon_status,sound},doc/"$PRGNAM"-"$VERSION"}
 }
 
-install -Dm 0644 appdata/face.svg "$install_root"/usr/share/pixmaps
-install -Dm 0644 appdata/face.desktop "$install_root"/usr/share/applications
-install -Dm 0644 icon_status/* "$install_root"/usr/share/face/icon_status
+install -Dm 0644 appdata/"$PRGNAM".svg "$install_root"/usr/share/pixmaps
+install -Dm 0644 appdata/"$PRGNAM".desktop "$install_root"/usr/share/applications
+install -Dm 0644 icon_status/* "$install_root"/usr/share/"$PRGNAM"/icon_status
+install -Dm 0644 sound/* "$install_root"/usr/share/"$PRGNAM"/sound
 
-cp -a ChangeLog LICENSE README.md "$install_root"/usr/doc/face-"$pkgver"
-cp -Tr src "$install_root"/usr/share/face
+cp -a ChangeLog LICENSE README.md "$install_root"/usr/doc/"$PRGNAM"-"$VERSION"
+cp -Tr src "$install_root"/usr/share/"$PRGNAM"
 
 echo "#!/bin/bash
-cd /usr/share/face
-python3 face.py" > "$install_root"/usr/bin/face
+cd /usr/share/$PRGNAM
+python3 main.py" > "$install_root"/usr/bin/"$PRGNAM"
 
-chmod 755 "$install_root"/usr/bin/face
+chmod 755 "$install_root"/usr/bin/"$PRGNAM"
 exit 0
